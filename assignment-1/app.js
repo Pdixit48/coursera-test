09a36c08a9ea4a18351111b581db830146161c1d
pankajdx48 <pankajdx48@gmail.com>
2017-05-04 10:45:26 +0530

    submitting assignment-1

--- assignment-1/app.js
+++ assignment-1/app.js
@@ -1,11 +1,29 @@
-(funnction(){
-  'use strict';
-  angular.module('myFirstApp',[])
-  .controller('myFirstController',Dowork);
+(function () {
+'use strict';
+angular.module('mod', [])
+.controller('LunchCheckController', fun1);
+fun1.$inject = ['$scope'];
+function fun1 ($scope) {
+  $scope.name = "";
+  $scope.enteredText="";
+  console.log($scope.enteredText);
+ $scope.checkWordsCount = function()
+ {
+ var separator =',';
+ var arrayOfStrings = $scope.enteredText.split(separator);
 
-  Dowork.$inject = ['$scope'];
-  funnction Dowork($scope){
-    $scope.name = "pankaj";
-    $scope.enteredText = "";
-  }
-}) ();
+   console.log('The original string is: "' + $scope.enteredText + '"');
+   console.log('The separator is: "' + separator + '"');
+   console.log('The array has ' + arrayOfStrings.length + ' elements: ' + arrayOfStrings.join(' / '));
+   if(arrayOfStrings.length > 3 && $scope.enteredText != ""){
+     $scope.enteredText = "Too much!";
+   }
+   else if ($scope.enteredText == "") {
+     $scope.enteredText = "Empty";
+   }
+   else{
+     $scope.enteredText = "Enjoy!";
+   }
+ }
+};
+})();
