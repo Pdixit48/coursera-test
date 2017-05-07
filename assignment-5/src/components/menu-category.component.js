dcbeed3a08dda293c728f9a0d4632254ed7cbdbf
pankajdx48 <pankajdx48@gmail.com>
2017-05-07 12:15:50 +0530

    submit remaining assignment

---
+++ assignment-5/src/components/menu-category.component.js
@@ -0,0 +1,14 @@
+(function () {
+"use strict";
+
+angular.module('public')
+.component('menuCategory', {
+  templateUrl: 'src/public/templates/menu-category.html',
+  bindings: {
+    category: '<'
+  }
+});
+
+
+
+})();
