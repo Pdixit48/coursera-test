0c343624efb673edf56ab3cea8f2327b0ff45383
pankajdx48 <pankajdx48@gmail.com>
2017-05-14 22:27:38 +0530

    assignment-5 added changes

---
+++ assignment-5/src/components/menu-category.component.js
@@ -0,0 +1,11 @@
+(function () {
+"use strict";
+
+angular.module('public')
+.component('menuCategory', {
+  templateUrl: 'src/public/templates/menu-category.html',
+  bindings: {
+    category: '<'
+  }
+});
+})();
